d583c7ba01be788aab29a1a5a8147061a66b6408
MarcoAranda <umauricio2705@gmail.com>
2023-06-25 15:30:49 -0500

    Promedio Funcional

--- src/app/dashboard/promedio/promedio.component.ts
+++ src/app/dashboard/promedio/promedio.component.ts
@@ -7,4 +7,19 @@ import { Component } from '@angular/core';
 })
 export class PromedioComponent {
 
+  promedio = {
+    numero1: null,
+    numero2 : null,
+    numero3 : null,
+    prm:0
+  }
+resultado=""
+
+  calcular(){
+    
+    this.promedio.prm= ((Number(this.promedio.numero1) + Number(this.promedio.numero1) + Number(this.promedio.numero1)) / 3)
+    this.resultado = "Su promedio es" + this.promedio.prm
+
+  }
+
 }
