a9a714d90b63cb8587542fc78a9743a522310e14
MarcoAranda <umauricio2705@gmail.com>
2023-06-25 14:28:18 -0500

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'DAWII_CL3_MARCO_ARANDA';
+}
