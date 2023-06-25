0ee1d939771666c1827e0d50c7b67303b98bbe39
MarcoAranda <umauricio2705@gmail.com>
2023-06-25 14:42:58 -0500

    Creacion de componente promedio

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { HomeComponent } from './dashboard/home/home.component';
+import { PromedioComponent } from './dashboard/promedio/promedio.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    HomeComponent,
+    PromedioComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/dashboard/promedio/promedio.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-promedio',
+  templateUrl: './promedio.component.html',
+  styleUrls: ['./promedio.component.scss']
+})
+export class PromedioComponent {
+
+}
